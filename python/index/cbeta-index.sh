0b8f89a98062a102d5c3c37e96a5bc47dd48e43d
Chris Wittern <chris@mbp>
2011-06-02 11:01:33 +0900

    added licence to Python files

--- python/index/cbeta-index.sh
+++ python/index/cbeta-index.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 #rm index.log
+mandokudir=~/db
 idxdir=/tmp/index
 coll=cbeta
 rm -Rf $idxdir/*/*.$coll.idx
-for subcoll in H J T W X
+#for subcoll in H J T W X
+for subcoll in S U P L M K G F C A 
 do
     cd $subcoll
     rm index.log
     for file in */*.txt
     do
 	echo $file $subcoll.$coll
-	python ~/projects/mandoku/mandoku_idx.py $file $subcoll.$coll
+	python $mandokudir/mandoku/python/index/mandoku_idx.py $file $subcoll.$coll
 	sleep 1
     done
     cd ..
