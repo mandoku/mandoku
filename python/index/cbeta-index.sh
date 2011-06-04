1fd52182d3a2be94488176f7f5eb57ea04adc31c
Christian Wittern <cwittern@gmail.com>
2011-06-04 20:26:59 +0900

    changes on dao

--- python/index/cbeta-index.sh
+++ python/index/cbeta-index.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 #rm index.log
-mandokudir=~/db
-idxdir=/tmp/index
+mandokudir=`dirname $0`
+idxdir=~/tmp/index
 coll=cbeta
-rm -Rf $idxdir/*/*.$coll.idx
+src=~/tmp/md
+cd $src
 #for subcoll in H J T W X
-for subcoll in S U P L M K G F C A 
+for subcoll in S U P L M K G F C A H J T W X
 do
+    rm -Rf $idxdir/*/*$subcoll.$coll.idx
     cd $subcoll
     rm index.log
     for file in */*.txt
     do
 	echo $file $subcoll.$coll
-	python $mandokudir/mandoku/python/index/mandoku_idx.py $file $subcoll.$coll
+	python $mandokudir/mandoku_idx.py $file $subcoll.$coll
 	sleep 1
     done
     cd ..
