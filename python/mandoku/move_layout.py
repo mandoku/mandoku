ca8b4f8518d9643489eb45a5c03beadbacab9890
Chris Wittern <chris@mba3>
2011-06-18 17:42:14 +0900

    Sat Jun 18 17:42:14 JST 2011

--- python/mandoku/move_layout.py
+++ python/mandoku/move_layout.py
@@ -1,17 +1,36 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-import mandoku
-reload(mandoku)
-f1 = mandoku.MandokuText('/Users/chris/db/text/cbeta/T/T01n0053/')
-f1.read()
-f2 = mandoku.MandokuText('/tmp/tkout/T01n0053.txt')
-f2.read()
-f=mandoku.MandokuComp(f1)
-f.setothertext(f2)
-f.move_pg_and_xb6tot2(0)
-f.segment(f1, f2)
-f2.sections = f2.newsections
-f2.defs['LASTPB'] = f1.defs['lastpb'].replace('<pb:', '<md:')
-f2.defs['title'] = f1.defs['title'].split()[-1]
-f2.write_to_sections('/tmp', True)
+import mandoku, os
+
+mddir="/Users/Shared/md/text/cbeta/T/"
+kdir = "/tmp/tkout/"
+outdir = "/tmp/mdout/"
+try:
+    os.rmdir(outdir)
+except:
+    pass
+try:
+    os.mkdir(outdir)
+except:
+    pass
+for f in os.listdir(kdir):
+    fid = f[:-4]
+    print f
+    f1 = mandoku.MandokuText(mddir + fid)
+    try:
+        f1.read()
+    except:
+        print "Error: ", f
+        continue
+    f2 = mandoku.MandokuText(kdir + f)
+    f2.read()
+    f=mandoku.MandokuComp(f1)
+    f.setothertext(f2)
+    f.move_pg_and_xb6tot2(0)
+    f.segment(f1, f2)
+    f2.sections = f2.newsections
+    f2.defs['LASTPB'] = f1.defs['lastpb'].replace('<pb:', '<md:')
+    f2.defs['title'] = f1.defs['title'].split()[-1]
+    mkdir(outdir + fid)
+    f2.write_to_sections(outdir + fid, True)
