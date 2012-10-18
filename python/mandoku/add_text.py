3306f81feee714abcf28d4b2de6a77c6e2cd037d
Chris Wittern <chris@mba3>
2012-10-18 17:14:08 +0900

    Thu Oct 18 17:14:08 JST 2012

--- python/mandoku/add_text.py
+++ python/mandoku/add_text.py
@@ -1,9 +1,9 @@
-import couchdb
-from mandoku import mandoku
+import couchdb, sys
 from mandoku import mandoku_couch
 server = couchdb.Server()
-db = server['md-tests']
+db = server['cbeta']
 m = server['meta']
-c1 = mandoku_couch.CouchMandoku(db, m, None, 100000, '/Users/chris/db/text/cbeta/T/T01n0005')
+file = sys.argv[1]
+c1 = mandoku_couch.CouchMandoku(db, m, None, 100000, file)
 c1.connectText()
 c1.addOtherBranches()
