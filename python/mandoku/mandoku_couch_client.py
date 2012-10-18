b1ea49ac781ccbd3f525a532706f78833ee37032
Chris Wittern <chris@mba3>
2012-10-18 15:42:09 +0900

    Thu Oct 18 15:42:09 JST 2012

--- python/mandoku/mandoku_couch_client.py
+++ python/mandoku/mandoku_couch_client.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import couchdb
+import couchdb, re
 from mandoku import mandoku
 from mandoku import mandoku_couch
 server = couchdb.Server()
@@ -20,4 +20,4 @@ res = db.view('_design/test/_view/ngram3', startkey=sk, endkey=ek)
 for k in res:
     docid, pos =k.value
     doc = db[doc]
-    s = "".join(["".join(z) for z in doc['seq'][pos-15:pos+15]])
+    s = re.sub(r'\n|\xb6', '', "".join(["".join(z) for z in doc['seq'][pos-15:pos+15]]))
