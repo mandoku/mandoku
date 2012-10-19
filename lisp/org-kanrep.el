9a25e87e089c547904db2eb997840b7dc09287e6
Chris Wittern <chris@mba3>
2012-10-19 20:26:20 +0900

    Fri Oct 19 20:26:20 JST 2012

--- lisp/org-kanrep.el
+++ lisp/org-kanrep.el
@@ -1,34 +1,30 @@
 ;; 
 ;; custom links to mandoku files from org
 ;; [2010-02-13T14:55:27+0900]  cwittern@gmail.com
+;; mandoku:cbeta:T52:0850a3::與道冥
 ;; [2012-10-19T20:11:32+0900]  created for kanrep from mandoku example
-;; 
+;; kanrep:filename:0850a3::與道冥
 
 (require 'org)
 (require 'mandoku)
 
-(org-add-link-type "kr" 'org-kanrep-open)
+(org-add-link-type "kanrep" 'org-kanrep-open)
 (add-hook 'org-store-link-functions 'org-kanrep-store-link)
-
+(defvar kanrep-base-dir (expand-file-name  "/Users/Shared/md/"))
 (defun org-kanrep-open (link)
-  "Open the text (and optionally go to indicated  position) in LINK.
-LINK will consist of a <textid> recognized by mandoku."
+  "Open the text (and optionally go to indicated  position) in LINK."
+
   ;; need to get the file name and then call mandoku-execute-file-search
-  (let ((coll (car (split-string link ":")))
-      (textid (car (cdr (split-string link ":"))))
-      (page (replace-in-string (car (cdr (cdr (split-string link ":")))) "_" ":" ))
+  (let ((filename (car (split-string link ":")))
+      (page (car (cdr (split-string link ":"))))
+;      (page (replace-in-string (car (cdr (cdr (split-string link ":")))) "_" ":" ))
       (src (car (cdr (split-string link "::")))))
   (message (format "%s" page))
-  (if (equal coll "meta")
-      ;; this does a headline only search in meta; we need to have the ID on the headline for this to work
-      (org-open-file (concat mandoku-meta-dir  textid ".org" ) t nil (concat "#" page)) 
-;      (message (format "%s" (concat mandoku-meta-dir  textid ".org" )))
-    (org-open-file (concat mandoku-text-dir coll "/" 
-    (funcall (intern (concat "mandoku-" coll "-textid-to-file")) textid page)) t nil 
+    (org-open-file (concat kanrep-base-dir filename) t nil 
    (if src 
-	(concat page "::" src)
+       (concat page "::" src)
      page)
-    ))))
+    )))
 
 
 
@@ -39,7 +35,7 @@ LINK will consist of a <textid> recognized by mandoku."
     (save-excursion
       (let* ((coll (mandoku-get-coll (buffer-file-name)))
 	     (location (mandoku-position-at-point))
-	     (link (concat "mandoku:" coll ":" location))
+	     (link (concat "kanrep:" coll ":" location))
 	     (description (mandoku-cit-format  location)))
 	(org-store-link-props
 	 :type "mandoku"
