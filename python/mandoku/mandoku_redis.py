afbdc7af71588ef0872236bda216d024d18dd407
Chris Wittern <chris@mbp3>
2012-12-04 16:39:27 +0900

    Tue Dec  4 16:39:27 JST 2012

--- python/mandoku/mandoku_redis.py
+++ python/mandoku/mandoku_redis.py
@@ -82,39 +82,6 @@ class RedisMandoku(MandokuText):
     def addNgram(self, action='add', n=3):
         #                    AddToRedis("".join([a[0] for a in chars[0:n]]), chars[0][1], defs['id'])
         p=self.r.pipeline()
-        # the end of the last sections:
-        lastpos = 0
-        for i in range(1, len(self.sections)+1):
-            s, f = self.sections[i-1]
-            try:
-                cnt = self.sections[i][0]
-            except(IndexError):
-                cnt = len(self.seq)
-            for j in range(s, cnt):
-                sx="".join([a[0] for a in self.seq[j:j+n]])
-                if j % 100 == 0:
-                    p.execute()
-                ##ngram:txt-sec, position
-                p.rpush("%s:%s"%(sx, f), j - lastpos)
-                if len(sx) == n:
-                    ##ngram, occurrences in txt
-                    p.zincrby(sx, self.txtid)
-                    try:
-                        p.zincrby(sx[0:2], sx[2])
-                    except:
-                        pass
-                    try:
-                        p.zincrby("%s-next" % (sx[0]), sx[1])
-                        p.zincrby("%s-prev" % (sx[1]), sx[2])
-                    except:
-                        pass
-            lastpos = s
-            p.execute()
-
-
-    def addNgramOld(self, action='add', n=3):
-        #                    AddToRedis("".join([a[0] for a in chars[0:n]]), chars[0][1], defs['id'])
-        p=self.r.pipeline()
         for i in range(1, len(self.sections)+1):
             s, f = self.sections[i-1]
             try:
