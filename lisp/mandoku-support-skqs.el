4d351a6daca57b9a5f2d1d41b896f5cf4c57e8cd
Chris Wittern <chris@mbp3>
2013-02-11 11:00:06 +0900

    Mon Feb 11 11:00:06 JST 2013

--- lisp/mandoku-support-skqs.el
+++ lisp/mandoku-support-skqs.el
@@ -1,9 +1,9 @@
-;;support file for the DIV texts.
+;;support file for the SKQS texts.
 ;; currently there are four subsections s1 s2 s3 s4, they are accessed through different functions
-(setq mandoku-div-titles nil)
-(setq mandoku-div-titletable (expand-file-name (concat mandoku-meta-dir "div-titles.txt")))
+(setq mandoku-skqs-titles nil)
+(setq mandoku-skqs-titletable (expand-file-name (concat mandoku-meta-dir "skqs-titles.txt")))
 
-(defun mandoku-div-page-to-image (locid)
+(defun mandoku-skqs-page-to-image (locid)
 "given a location id, returns the path of the image"
 (let* ((textid (car (split-string locid ":" )))
        (vol (substring textid 0 3))
@@ -14,11 +14,11 @@
 
 
 
-(defun mandoku-div-vol-page (vol page)
+(defun mandoku-skqs-vol-page (vol page)
 (interactive "sPlease enter the volume, including a letter (T X J H W), eg T51: \nsPlease enter the page with optional line number like 439a12: ")
-(org-mandoku-open (concat "div: " vol ":"  page)))
+(org-mandoku-open (concat "skqs: " vol ":"  page)))
 
-(defun mandoku-div-parse-location (loc)
+(defun mandoku-skqs-parse-location (loc)
 "parses the location in the index file, returns a list starting with vol (=sec) page line.
 For compatibility with the other collections, we move the text number to the back."
 (list (format "n%4.4d" (string-to-number (car (split-string loc "[:_]")))) 
@@ -27,42 +27,42 @@ For compatibility with the other collections, we move the text number to the bac
       (nth 4 (split-string loc "[:_]"))))
 
 
-(defun mandoku-div-textid-to-title (textid page)
+(defun mandoku-skqs-textid-to-title (textid page)
 "returns the title of the text"
 ;; read the table if necessary
-(mandoku-div-read-titletable)
-(list textid (gethash textid mandoku-div-titles))
+(mandoku-skqs-read-titletable)
+(list textid (gethash textid mandoku-skqs-titles))
 )
 
-(defun mandoku-div-textid-to-file (textid page)
+(defun mandoku-skqs-textid-to-file (textid page)
 "Textids of the form T01n0001 and page strings like 439a12 are used to find the right file"
   (let* ((subcoll (substring textid 0 2))
 	 (textnum (string-to-number (car (cdr (split-string (substring textid 2) "n")))))
 	 ;; the page I get passed in from org-mandoku is separated with :
 	 (sec (string-to-number (car (split-string page ":")))))
-    (mandoku-div-vol-page-to-file subcoll textnum sec)))
+    (mandoku-skqs-vol-page-to-file subcoll textnum sec)))
 
-(defun mandoku-div-vol-page-to-file (subcoll textnum sec)
+(defun mandoku-skqs-vol-page-to-file (subcoll textnum sec)
 "converts the text id and page to a file name.  Page is numeric with the ending abc as 123"
 (let ((txtid (concat (upcase subcoll) "n" (format "%4.4d" textnum)))) 
 (concat (downcase subcoll) "/" txtid "/" txtid "-" (format "%3.3d" sec) ".txt")
 ))
 
 
-(defun mandoku-div-vol-page-to-title  (subcoll vol page)
+(defun mandoku-skqs-vol-page-to-title  (subcoll vol page)
   (list subcoll (concat vol ":" page))
 )
 
 
-(defun mandoku-div-read-titletable () 
+(defun mandoku-skqs-read-titletable () 
 "read the titles table"
-  (when (file-exists-p mandoku-div-titletable)
-    (unless mandoku-div-titles
-      (setq mandoku-div-titles (make-hash-table :test 'equal))
+  (when (file-exists-p mandoku-skqs-titletable)
+    (unless mandoku-skqs-titles
+      (setq mandoku-skqs-titles (make-hash-table :test 'equal))
       (with-temp-buffer
         (let ((coding-system-for-read 'utf-8)
               textid)
-          (insert-file-contents mandoku-div-titletable)
+          (insert-file-contents mandoku-skqs-titletable)
           (goto-char (point-min))
           (while (re-search-forward "^\\([a-z0-9]+\\)	\\([^	]+\\)" nil t)
-	     (puthash (match-string 1) (match-string 2) mandoku-div-titles)))))))
+	     (puthash (match-string 1) (match-string 2) mandoku-skqs-titles)))))))
