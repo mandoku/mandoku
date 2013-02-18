289dc38bfb74aacfd4aa10582cc0b2e3d6914891
Chris Wittern <chris@mbp3>
2013-02-18 16:28:07 +0900

    Mon Feb 18 16:28:07 JST 2013

--- lisp/mandoku-export.el
+++ lisp/mandoku-export.el
@@ -1,10 +1,11 @@
 ;; export backends for mandoku files
 
-(org-export-define-derived-backend md-latex latex
-  :translate-alist ((headline . md-latex-headline-translator)
-                    (template . md-latex-template)))
+(org-export-define-derived-backend mandoku-latex latex
+  :filters-alist ((:filter-parse-tree . mandoku-separate-elements))
+  :translate-alist ((headline . mandoku-latex-headline-translator)
+                    (template . mandoku-latex-template)))
 
 
-(org-export-define-derived-backend md-odt odt
-  :translate-alist ((headline . md-odt-headline-translator)
-                    (template . md-odt-template)))
+(org-export-define-derived-backend mandoku-odt odt
+  :translate-alist ((headline . mandoku-odt-headline-translator)
+                    (template . mandoku-odt-template)))
