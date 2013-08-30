63c674a18a7663f5a7d68082829e67ac007c1b2a
Chris Wittern <chris@mbp3>
2013-08-30 11:56:06 +0900

    Fri Aug 30 11:56:06 JST 2013

--- lisp/mandoku-remote.el
+++ lisp/mandoku-remote.el
@@ -5,11 +5,12 @@
 (require 'url-http)
 ;; probably best to make this a list sometime...
 (defvar mandoku-remote-url "http://localhost:5000/search")
-
+(setq mandoku-remote-url "http://127.0.0.1:5000/search")
 (defun mandoku-do-query (type str)
       (let ((url-request-method "GET")
+	    ;; dolist or mapconcat...
             (arg-stuff (concat "?query=" (url-hexify-string str)
                          "&filter=" (url-hexify-string type))))
-        (url-retrieve (concat mandoku-remote-url arg-stuff)
+        (url-retrieve (concat mandoku-remote-url)
                       (lambda (status) (switch-to-buffer (current-buffer))))))
 
