e00ec714a600058bde292ba4a7adc80c7e015cb1
Chris Wittern <chris@mbp3>
2013-09-05 18:00:07 +0900

    Thu Sep  5 18:00:07 JST 2013

--- lisp/mandoku-init.el
+++ lisp/mandoku-init.el
@@ -2,13 +2,15 @@
 (require 'mandoku)
 (require 'org-mandoku)
 
-(dolist (dir (directory-files mandoku-text-dir nil "^[^.,].*"))
-  (when (file-directory-p (concat mandoku-text-dir dir))
-    (load (concat "mandoku-support-" dir) t )))
+;; (dolist (dir (directory-files mandoku-text-dir nil "^[^.,].*"))
+;;   (when (file-directory-p (concat mandoku-text-dir dir))
+;;     (load (concat "mandoku-support-" dir) t )))
 
 ;(setq mandoku-catalogs-alist '(("ZB6 佛部" . "/Users/chris/projects/meta/zb-cbeta.org")))
 
-
+(dolist (dir (directory-files mandoku-meta-dir nil "^[^.,].*"))
+  (when (file-directory-p (concat mandoku-text-dir dir))
+    
 
 ;(dolist (x mandoku-catalogs-alist)
 ;  (message "%s" (cdr x)))
