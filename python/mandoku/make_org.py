40c306474f0f8fb47a979aa7899df0a7c88d1c43
Chris Wittern <chris@mbp3>
2013-09-11 20:56:07 +0900

    Wed Sep 11 20:56:07 JST 2013

--- python/mandoku/make_org.py
+++ python/mandoku/make_org.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
 
-import os, sys, codecs
+import os, sys, codecs, re, git
 
 def proctxt(arg, dirname, names):
-    names = names.sort()
-    fn = dirname.split('/')[-1]
-    of = codecs.open("%s/%s.org" % (dirname, fn), 'w', 'utf-8')
-    for f in names:
-        if f.endswith('txt'):
-            
-    
+    if not (".git" in dirname):
+        fn = dirname.split('/')[-1]
+        of = codecs.open("%s/%s.org" % (dirname, fn), 'w', 'utf-8')
+        cnt = 0
+        nrep = git.Repo.init(dirname)
+        for f in names:
+            if f.endswith('txt'):
+                cnt += 1
+                inf = codecs.open("%s.%s" % (dirname, f), 'r', 'utf-8')
+                for line in inf:
+                    if line.startswith('#+TITLE:') and cnt == 1:
+                        of.write(line)
+                    elif "JUAN" in line:
+                        j = line[line.index('JUAN')+5:-1]
+                        of.write("[[%s][%s]]\n" % (f, j))
+                        break
+
+        nrep.index.add([fn +'.org'])
+        nrep.index.commit("Added index file %s." % (fn+'.org'))  
+os.path.walk('.', proctxt, "")
+                             
 
 
