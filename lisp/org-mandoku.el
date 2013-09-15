49d646ae67f3fcaeec8fb6d36cd6ad414f2ca369
Chris Wittern <chris@mbp3>
2013-09-15 18:19:58 +0900

    Sun Sep 15 18:19:58 JST 2013

--- lisp/org-mandoku.el
+++ lisp/org-mandoku.el
@@ -29,10 +29,10 @@ LINK will consist of a <textid> recognized by mandoku."
 				  (funcall (intern (concat "mandoku-" coll "-textid-to-file")) textid page)
 				(concat (substring coll 0 4) "/" (substring coll 0 8) "/" coll)))
 		   )))
-    (message (format "%s" page))
+;    (message (format "%s" page))
     (if (equal coll "meta")
 	  ;; this does a headline only search in meta; we need to have the ID on the headline for this to work
-	  (org-open-file filename  t nil (concat "" textid)) 
+	  (org-open-file filename  t nil (concat "#" textid)) 
 					;      (message (format "%s" (concat mandoku-meta-dir  textid ".org" )))
       (if (file-exists-p (concat mandoku-text-dir "/" filename))
 	  (org-open-file (concat mandoku-text-dir "/" filename) t nil 
