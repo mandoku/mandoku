9e7ec99b6eb94128e348311bb3125f35fe5dde7d
Chris Wittern <chris@mbp3>
2013-09-15 14:59:59 +0900

    Sun Sep 15 14:59:59 JST 2013

--- python/mandoku/change_org.py
+++ python/mandoku/change_org.py
@@ -7,21 +7,33 @@ def proctxt(arg, dirname, names):
         x = "".join(names)
         if "txt" in x:
             nrep = git.Repo.init(dirname)
+            f = dirname.split('/')[-1]
+            ofn = f[:-3] + 'bak'
+            os.rename(dirname + '/'+ f, dirname + '/' + ofn)
+            nf = f
+            of=codecs.open(dirname + '/' + nf, 'w', 'utf-8')
+            inf = codecs.open("%s/%s" % (dirname, ofn), 'r', 'utf-8')
+            for line in inf:
+                line = re.sub("file:", "mandoku:", line)
+                of.write(line)
+            of.write("\n* 卷\n")
             for f in names:
-                if f.endswith('org'):
-                    ofn = f[:-3] + 'bak'
-                    os.rename(dirname + '/'+ f, dirname + '/' + ofn)
-                    nf = f
-                    of=codecs.open(dirname + '/' + nf, 'w', 'utf-8')
-                    inf = codecs.open("%s/%s" % (dirname, ofn), 'r', 'utf-8')
+                if f.endswith('txt'):
+                    inf = codecs.open("%s/%s" % (dirname, f), 'r', 'utf-8')
                     for line in inf:
-                        line = re.sub("file:", "mandoku:", line)
-                        of.write(line)
-                    of.close()
-                    inf.close()
-                    os.remove(dirname + '/' + ofn)
-                    nrep.index.add([nf])
-                    nrep.index.commit("Changed index file %s." % (nf))
+                        if line.startswith('#+TITLE:'):
+                            title = line[line.index('TITLE')+6:-1]
+                        elif "JUAN" in line:
+                            j = line[line.index('JUAN')+5:-1]
+                            j = re.sub(ur'[\[\]]', '', j)
+                            of.write("** [[mandoku:%s][%s %s]]\n" % (f, title.strip(), j))
+                            break
+
+            of.close()
+            inf.close()
+            os.remove(dirname + '/' + ofn)
+            nrep.index.add([nf])
+            nrep.index.commit("Changed index file %s." % (nf))
 os.path.walk('.', proctxt, "")
                              
 
