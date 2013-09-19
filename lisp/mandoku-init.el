babdcd7510aeb5fa24a1df48626816ee5dc8006b
Chris Wittern <chris@mba3>
2013-09-19 14:42:04 +0900

    Thu Sep 19 14:42:04 JST 2013

--- lisp/mandoku-init.el
+++ lisp/mandoku-init.el
@@ -18,7 +18,7 @@
 (setq mandoku-do-remote t)
 
 (mandoku-read-titletables) 
-;(setq mandoku-base-dir (expand-file-name  "/Users/Shared/md/"))
+;(setq mandoku-base-dir (expand-file-name  "/Users/Shared/md-remote/"))
 (setq mandoku-image-dir (expand-file-name  (concat mandoku-base-dir "images/")))
 (setq mandoku-index-dir (expand-file-name  (concat mandoku-base-dir "index/")))
 
