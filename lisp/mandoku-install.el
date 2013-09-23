bc9206718867f01bd0edf36cd7571165562ddeeb
Chris Wittern <chris@mbp3>
2013-09-23 12:12:05 +0900

    Mon Sep 23 12:12:05 JST 2013

--- lisp/mandoku-install.el
+++ lisp/mandoku-install.el
@@ -4,90 +4,75 @@
 
 (defvar mandoku-git-install-url "http://github.com/cwittern/mandoku.git")
 
-(defun mandoku-install ()
-(let ((mandoku-root
-       (file-name-as-directory
-	(or (bound-and-true-p mandoku-base-dir)
-	    (concat (file-name-as-directory user-emacs-directory) "mandoku")))))
+(defun mandoku-install (&optional mandoku-base-dir)
+  (let ((mandoku-root
+	 (file-name-as-directory
+	  (or (bound-and-true-p mandoku-base-dir)
+	      (concat (file-name-as-directory user-emacs-directory) "mandoku")))))
+    
+    (setq mandoku-base-dir mandoku-root)
 
-  (setq mandoku-base-dir mandoku-root)
-
-  (when (file-directory-p mandoku-root)
-    (add-to-list 'load-path (concat mandoku-root "mandoku/lisp")))
+    (when (file-directory-p mandoku-root)
+      (add-to-list 'load-path (concat mandoku-root "mandoku/lisp")))
 
   ;; try to require mandoku, failure means we have to install it
-  (unless (require 'mandoku nil t)
-    (unless (file-directory-p mandoku-root)
-      (make-directory mandoku-root t))
-    
-    (let* ((package   "mandoku")
-	   (buf       (switch-to-buffer "*mandoku bootstrap*"))
-	   (pdir      (file-name-as-directory (concat mandoku-root package "/lisp")))
-	   (git       (or (executable-find "git")
-			  (error "Unable to find `git'")))
-	   (url       (or (bound-and-true-p mandoku-git-install-url)
+    (unless (require 'mandoku nil t)
+      (unless (file-directory-p mandoku-root)
+	(make-directory mandoku-root t))
+      
+      (let* ((package   "mandoku")
+	     (buf       (switch-to-buffer "*mandoku bootstrap*"))
+	     (pdir      (file-name-as-directory (concat mandoku-root package "/lisp")))
+	     (git       (or (executable-find "git")
+			    (error "Unable to find `git'")))
+	     (url       (or (bound-and-true-p mandoku-git-install-url)
 			  "http://github.com/cwittern/mandoku.git"))
-	   (default-directory mandoku-root)
-	   (process-connection-type nil)   ; pipe, no pty (--no-progress)
+	     (default-directory mandoku-root)
+	     (process-connection-type nil)   ; pipe, no pty (--no-progress)
 
 	   ;; First clone mandoku
-	   (status
-	    (call-process
-	     git nil `(,buf t) t "--no-pager" "clone" "-v" url package)))
+	     (status
+	      (call-process
+	       git nil `(,buf t) t "--no-pager" "clone" "-v" url package)))
 
-      (unless (zerop status)
-	(error "Couldn't clone mandoku from the Git repository: %s" url))
+	(unless (zerop status)
+	  (error "Couldn't clone mandoku from the Git repository: %s" url))
 
-      ;; switch branch if we have to
-      (let* ((branch (cond
-                      ;; Check if a specific branch is requested
-                      ((bound-and-true-p mandoku-install-branch))
-                      ;; Check if master branch is requested
-                      ((boundp 'mandoku-master-branch) "master")
-                      ;; As a last resort, use the master branch
-                      ("master")))
-             (remote-branch (format "origin/%s" branch))
-	     (default-directory mandoku-root)
-             (bstatus
-               (if (string-equal branch "master")
-                 0
-                 (call-process git nil (list buf t) t "checkout" "-t" remote-branch))))
-        (unless (zerop bstatus)
-          (error "Couldn't `git checkout -t %s`" branch)))
+	;; switch branch if we have to
+	(let* ((branch (cond
+			;; Check if a specific branch is requested
+			((bound-and-true-p mandoku-install-branch))
+			;; Check if master branch is requested
+			((boundp 'mandoku-master-branch) "master")
+			;; As a last resort, use the master branch
+			("master")))
+	       (remote-branch (format "origin/%s" branch))
+	       (default-directory mandoku-root)
+	       (bstatus
+		(if (string-equal branch "master")
+		    0
+		  (call-process git nil (list buf t) t "checkout" "-t" remote-branch))))
+	  (unless (zerop bstatus)
+	    (error "Couldn't `git checkout -t %s`" branch)))
 
-      (add-to-list 'load-path pdir)
-      (load package)
+	(add-to-list 'load-path pdir)
+	(load package)
 
-      (unless (file-directory-p mandoku-meta-dir)
-	(make-directory mandoku-meta-dir t))
-      (unless (file-directory-p mandoku-sys-dir)
-	(make-directory mandoku-sys-dir t))
-      (unless (file-directory-p mandoku-temp-dir)
-	(make-directory mandoku-temp-dir t))
+	(unless (file-directory-p mandoku-meta-dir)
+	  (make-directory mandoku-meta-dir t))
+	(unless (file-directory-p mandoku-sys-dir)
+	  (make-directory mandoku-sys-dir t))
+	(unless (file-directory-p mandoku-temp-dir)
+	  (make-directory mandoku-temp-dir t))
+	
 
-
-      (let ((byte-compile-warnings nil)
-	    ;; Byte-compile runs emacs-lisp-mode-hook; disable it
-	    (file pdir)
-	    emacs-lisp-mode-hook)
-	(byte-recompile-directory file 0))
-
-
-;;
-      (let* ((pdir (file-name-as-directory mandoku-meta-dir))
-	    (url       (or (bound-and-true-p mandoku-catalog-clone-url)
-			  "http://github.com/cwittern/ZB"))
-	    (default-directory pdir)
-	    
-	   ;; Now clone the catalogs
-	   (status
-	    (call-process
-	     git nil `(,buf t) t "--no-pager" "clone" "-v" url)))
-        (unless (zerop status)
-	  (error "Couldn't clone mandoku catalogs from the Git repository: %s" url)))
-      
-
-
+	(let ((byte-compile-warnings nil)
+	      ;; Byte-compile runs emacs-lisp-mode-hook; disable it
+	      (file pdir)
+	      emacs-lisp-mode-hook)
+	  (byte-recompile-directory file 0)))
+      ;; TODO to loop over the repository list, get the clone URL there and clone the catalog
+      (mandoku-clone-catalog "http://github.com/cwittern/ZB")
       (load "mandoku-init")
 
       (mandoku-update-subcoll-list)
@@ -96,19 +81,19 @@
       (mandoku-read-lookup-list)
       (with-current-buffer buf
 	(goto-char (point-max))
-	(insert "\nCongrats, mandoku is installed and ready to serve!"))))))
+	(insert "\nCongrats, mandoku is installed and ready to serve!")))))
+
+
+(defun mandoku-clone-catalog (url)
+      (let* ((default-directory (file-name-as-directory mandoku-meta-dir))
+	   ;; Now clone the catalogs
+	   (status
+	    (call-process
+	     git nil `(,buf t) t "--no-pager" "clone" "-v" url)))
+        (unless (zerop status)
+	  (error "Couldn't clone mandoku catalogs from the Git repository: %s" url))))
+      
 
 
 
-;; (defun mandoku-post-install (package)
-;;   "install the other necessary files, make the system files etc."
-;;   (let*  (
-;; 	   (buf       (switch-to-buffer "*mandoku bootstrap*"))
-;; 	   (pdir      (file-name-as-directory (concat mandoku-root package)))
-;; 	   (git       (or (executable-find "git")
-;; 			  (error "Unable to find `git'")))
-;; 	   (url       (or (bound-and-true-p mandoku-git-install-url)
-;; 			  "http://github.com/cwittern/mandoku.git"))
-;; 	   (default-directory mandoku-root)
-;; 	   (process-connection-type nil)   ; pipe, no pty (--no-progress)
-
+;; mandoku-install ends here
