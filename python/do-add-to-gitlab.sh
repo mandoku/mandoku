b4dcb034ef99084e2cbab4e9831c50e8089d43bf
Chris Wittern <chris@mbp3>
2013-10-01 11:14:06 +0900

    Tue Oct  1 11:14:06 JST 2013

--- python/do-add-to-gitlab.sh
+++ python/do-add-to-gitlab.sh
@@ -11,9 +11,10 @@ do
 	    if [ -d $d2]
 		then
 		cd $d2
-		tit = `echo head -1 $d2.org | sed -e "s/TITLE//"`
+		tit = `head -1 ZB1a0001.org | sed -e "s/#+TITLE: //"`
 		curl --connect-timeout 60 --header "PRIVATE-TOKEN: $privtok" -X POST "http://test.kanripo.org/gitlab/api/v3/projects?name=$d2&description=$tit" >> $remotelog
-		
+		git remote add origin git@zb.kanripo.org:zb/$df.git
+		git push remote 
 		cd ..
 	    fi
 	cd ..
