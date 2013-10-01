80da45d854fda78f4146373a1bfd4275dba528bb
Chris Wittern <chris@mbp3>
2013-10-01 10:56:08 +0900

    Tue Oct  1 10:56:08 JST 2013

--- python/do-add-to-gitlab.sh
+++ python/do-add-to-gitlab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 privtok=hzj3TXjgzysKdqcVPmSF
-
+remotelog=/tmp/gitlab-rem.log
 for d1 in ZB*
 do
     if [ -d $d1]
@@ -11,6 +11,8 @@ do
 	    if [ -d $d2]
 		then
 		cd $d2
+		tit = `echo head -1 $d2.org | sed -e "s/TITLE//"`
+		curl --connect-timeout 60 --header "PRIVATE-TOKEN: $privtok" -X POST "http://test.kanripo.org/gitlab/api/v3/projects?name=$d2&description=$tit" >> $remotelog
 		
 		cd ..
 	    fi
