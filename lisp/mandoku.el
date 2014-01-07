675d12765e3d088c8959392d5beb69e882058275
Chris Wittern <chris@mbp3>
2014-01-07 15:32:32 +0900

    Tue Jan  7 15:32:32 JST 2014

--- lisp/mandoku.el
+++ lisp/mandoku.el
@@ -1216,7 +1216,7 @@ Letters do not insert themselves; instead, they are commands.
 
 (defun mandoku-update()
   (interactive)
-;  (mandoku-update-internal "/mandoku/lisp")
+  (mandoku-update-internal "/mandoku/lisp")
   (mandoku-update-internal "/meta/ZB"))
 
 (defun mandoku-update-internal (package)
