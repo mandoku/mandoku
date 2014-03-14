5ab40e099bfc170261eb61d17cc65618863154e6
Chris Wittern <chris@mbp3>
2014-03-14 12:08:55 +0900

    Fri Mar 14 12:08:55 JST 2014

---
+++ python/mandoku/debug.py
@@ -0,0 +1,16 @@
+sys.path.insert(0, '/Users/chris/db/mandoku/python/mandoku')
+import mandoku, os
+mddir="/Users/Shared/md/text/cbeta/T/"
+kdir = "/tmp/tkout/"
+outdir = "/tmp/mdout/"
+
+f = "T01n0001.txt"
+fid = f[:-4]
+f1 = mandoku.MandokuText(mddir + fid)
+f1.read()
+f2 = mandoku.MandokuText(kdir + f)
+f2.read()
+f=mandoku.MandokuComp(f1)
+f.setothertext(f2)
+f.move_pg_and_xb6tot2(0)
+f.segment(f1, f2)
