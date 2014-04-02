127e54211c8e851c4eae715487eb89ff7cc9e556
Chris Wittern <chris@mbp3>
2014-04-02 18:00:06 +0900

    Wed Apr  2 18:00:06 JST 2014

--- python/mandoku/md_varlist.py
+++ python/mandoku/md_varlist.py
@@ -6,14 +6,15 @@ user=getpass.getuser()
 sys.path.insert(0, '/Users/' + user + '/db/mandoku/python/mandoku')
 
 
-import mandoku, os, git
+import mandoku, os, git, codecs
 
 
 def prepare(textpath=".", src="master", n=3):
     """Move the layout from the srcbranch to the targetbranch. If
     targetbranch is None, use the current branch."""
+    l = []
     f1 = mandoku.MandokuText(textpath, src)
-    rd = os.realpath(textpath)
+    rd = os.path.realpath(textpath)
     wp = rd + '.wiki'
     try:
         wiki = git.Repo(wp)
@@ -28,12 +29,12 @@ def prepare(textpath=".", src="master", n=3):
     f1.read()
     f1.add_metadata()
     f1.addOtherBranches()
-    s=f1.seq
+    seq=f1.seq
     r = f1.branches
     br = r.keys()
     pp = f1.pages.keys()
-    for i in range(1, len(self.sections)+1):
-        s, f = self.sections[i-1]
+    for i in range(1, len(f1.sections)+1):
+        s, f = f1.sections[i-1]
         of = codecs.open(wp + '/varlist/' + f, 'w', 'utf-8') 
         start = s
         try:
@@ -42,7 +43,7 @@ def prepare(textpath=".", src="master", n=3):
             end = len(f1.seq)
         for j in range(start, end):
             varflag = 0
-            if pp.has_key(j):
+            if j in pp:
                 currentpage=f1.pages[j]
                 l = f1.lines[currentpage]
                 ll = 1
@@ -53,7 +54,8 @@ def prepare(textpath=".", src="master", n=3):
                 if r[v].has_key(j):
                     if varflag == 0:
                         varflag = 1
-                        of.write("** %s%2.2d %s *%s* %s\n" % (currentpage.split('_')[-1][:-1], ll, s[j-n:j], s[j], s[j+n]))
+                        print currentpage, ll, seq[j]
+#                        of.write("** %s%2.2d %s *%s* %s\n" % (currentpage.split('_')[-1][:-1], ll, s[j-n:j], s[j], s[j+n]))
                     of.write( " %s: %s\n" % (v, "".join(r[v][j])))
         of.close()
 
