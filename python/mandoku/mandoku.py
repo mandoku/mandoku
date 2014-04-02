aa87b9eb082708527df2f4b06767a77fbf017872
Chris Wittern <chris@mbp3>
2014-04-02 12:03:32 +0900

    Wed Apr  2 12:03:32 JST 2014

--- python/mandoku/mandoku.py
+++ python/mandoku/mandoku.py
@@ -183,7 +183,7 @@ function with access to a database."""
                         m += e
                     else:
                         for j in range(0, len(e)):
-                            if e[j] in (u'〈', u'《', u'「', u'『', u'【', u'〖', u'〘', u'〚', u'\u3000', '(', '*', ' '):
+                            if e[j] in (u'〈', u'《', u'「', u'『', u'【', u'〖', u'〘', u'〚', u'\u3000', '(', '*', '.', ' '):
                                 p += e[j]
                             elif re.search(r"[0-9]", e[j]):
                                 p += e[j]
