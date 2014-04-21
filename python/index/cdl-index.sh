33d936bdeae2839662742193c3921ed1630af5df
Chris Wittern <chris@mbp3>
2014-04-21 16:14:04 +0900

    Mon Apr 21 16:14:04 JST 2014

--- python/index/cdl-index.sh
+++ python/index/cdl-index.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-mandokudir=`dirname $0`
-idxdir=/Users/Shared/md-remote/index
+mandokudir=/Users/chris/db/mandoku/python/index
+idxdir=/tmp/zb5-index
 mkdir -p $idxdir
 coll=krp
 src=/Users/Shared/md-remote/text/
@@ -9,7 +9,7 @@ for subcoll in ZB5a ZB5b ZB5c ZB5d ZB5e ZB5f ZB5g ZB5h
 do
     cd $subcoll
     rm index.log
-    for file in ZB6q0002/*.txt
+    for file in ZB5[a-z0-9]*/*.txt
     do
 	echo $file $subcoll.$coll
 	python $mandokudir/mandoku_idx.py $file $subcoll.$coll $idxdir
