818fcc78442c0020b7305beb63b82d89aaf5b6d0
Chris Wittern <chris@mbp3>
2014-06-06 14:28:05 +0900

    Fri Jun  6 14:28:05 JST 2014

--- lisp/mandoku-annot.el
+++ lisp/mandoku-annot.el
@@ -9,14 +9,14 @@
 
 (defun mandoku-annot-scan (&optional annot-dir)
 (interactive)
-(let (f1 type p)
+(let (f1 type p olp)
   (save-excursion 
     (goto-char (point-min))
     (while (re-search-forward mandoku-annot-regex nil t)
       (setq f1 (buffer-substring-no-properties (match-beginning 1) (match-end 1)))
       (setq type (buffer-substring-no-properties (match-beginning 2) (match-end 2)))
       (setq p (mandoku-position-with-char  (search-backward f1)))
-      
+      (setq olp (mandoku-get-outline-path p))
     
 
 
