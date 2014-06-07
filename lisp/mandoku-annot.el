3d185a3511188987b4796c75c7c500c4fe61b488
Chris Wittern <chris@mbp3>
2014-06-07 12:14:06 +0900

    Sat Jun  7 12:14:06 JST 2014

--- lisp/mandoku-annot.el
+++ lisp/mandoku-annot.el
@@ -5,7 +5,14 @@
 
 (defvar mandoku-annot-dir (expand-file-name  (concat mandoku-base-dir "notes/")))
 
-(defvar mandoku-annot-regex "^\\([^ ]+\\) *\\([^:]+\\)::\\(.*?\\)$") 
+(defvar mandoku-annot-regex "^\\([^:\n ]+\\) *\\([^:]+\\)::\\(.*?\\)$") 
+
+(defun mandoku-location-put (&rest stuff)
+  "Add properties to the capture property list `mandoku-location-plist'"
+  (while stuff
+    (setq mandoku-location-plist (plist-put mandoku-location-plist
+				       (pop stuff) (pop stuff)))))
+
 
 (defun mandoku-annot-scan (&optional annot-dir)
   (interactive)
@@ -16,12 +23,16 @@
 	(setq f1 (buffer-substring-no-properties (match-beginning 1) (match-end 1)))
 	(setq mandoku-location-plist :context f1)
 	(mandoku-location-put 
-	 :location (concat (mandoku-position-with-char  (search-backward f1)) "+" (number-to-string (length f1) ))
+	 :location (concat (mandoku-position-with-char  
+			    (search-backward f1))
+			   "+" (number-to-string (length f1) ))
 	 :olp (mandoku-get-outline-path p)
 	 :type (buffer-substring-no-properties (match-beginning 2) (match-end 2)))
 	(mandoku-annot-insert)
 	))))      
     
 
+(defun mandoku-annot-insert ()
+)
 
 (provide 'mandoku-annot)
