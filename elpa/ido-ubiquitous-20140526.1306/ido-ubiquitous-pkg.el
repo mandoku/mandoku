cc386a7ab1c52fd0ff2171d3e6ab6869663839d3
Chris Wittern <chris@mbp3>
2014-06-19 14:05:19 +0900

    trying to fix some errors due to wrong .gitignore

--- elpa/ido-ubiquitous-20140526.1306/ido-ubiquitous-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ido-ubiquitous" "20140526.1306" "Use ido (nearly) everywhere." '((emacs "24.1")) :url "https://github.com/DarwinAwardWinner/ido-ubiquitous" :keywords ("convenience"))
--- elpa/org-plus-contrib-20140616/org-plus-contrib-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-plus-contrib" "20140616" "Outline-based notes management and organizer" 'nil)
