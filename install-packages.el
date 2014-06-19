a79604cbeee3129dbf6d598a2a3afa2a3fcded45
Chris Wittern <chris@mbp3>
2014-06-19 10:56:06 +0900

    Thu Jun 19 10:56:06 JST 2014

---
+++ install-packages.el
@@ -0,0 +1,40 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;; Package manager
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'cl)
+(require 'package)
+
+(setq package-archives
+      '(("org"         . "http://orgmode.org/elpa/")
+	("gnu"         . "http://elpa.gnu.org/packages/")
+;	("original"    . "http://tromey.com/elpa/")	
+	("melpa" . "http://melpa.milkbox.net/packages/")
+;	("marmalade"   . "http://marmalade-repo.org/packages/")
+	))
+
+(setq package-user-dir (expand-file-name "elpa"  starter-kit-dir))
+
+(package-initialize)
+
+(defvar starter-kit-packages
+  (list 'flx-ido 'ido-ubiquitous 'smex
+        'yasnippet
+        'magit 
+	'bbdb 'bbdb-ext
+        'auctex 'reftex
+        'undo-tree
+	'diminish
+	;'icicles
+	'org 'org-plus-contrib
+	'elpy
+        'rainbow-mode)
+  "Libraries that should be installed by default.")
+
+(unless (every #'package-installed-p starter-kit-packages)
+  (package-refresh-contents)
+  (dolist (package starter-kit-packages)
+    (unless (package-installed-p package)
+      (message "installing %s" package)
+      (package-install package))))
+
+(provide 'install-packages)
