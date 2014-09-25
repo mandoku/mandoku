a66f07b7c3ce75fb2df2742ce8a2d5ea6d4a65fc
Chris Wittern <chris@mbp3>
2014-09-25 14:16:26 +0900

    deleted for windows bundle

--- python/add_text.py
+++
@@ -1,9 +0,0 @@
-import couchdb, sys
-from mandoku import mandoku_couch
-server = couchdb.Server()
-db = server['cbeta']
-m = server['meta']
-file = sys.argv[1]
-c1 = mandoku_couch.CouchMandoku(db, m, None, 100000, file)
-c1.connectText()
-c1.addOtherBranches()
