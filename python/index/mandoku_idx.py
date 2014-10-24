0b9a9cce7a9dfc1c0aa11d66716226020289cbc9
Chris Wittern <chris@mbp3>
2014-10-24 12:50:04 +0900

    fixing a few bugs.  Still need to look 2 shrt note

--- python/index/mandoku_idx.py
+++ python/index/mandoku_idx.py
@@ -6,13 +6,18 @@ from __future__ import absolute_import
 import os, sys, codecs, re, datetime, os.path, git
 from difflib import *
 
+debug = True
 ch_re = re.compile(ur'(\[[^\]]*\]|&[^;]*;|&amp;[CZ][X3-7]-[A-F0-9]+|.)')
 img_re = re.compile(ur'<i[^>]*>')
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 outfiles = {}
 tab={'a':'1', 'b':'2', 'c':'3', 'd': '4', 'e': '5', 'f': '6', 'g':'7', 'h':'8', 'i':'9'}
 idx={}
+pcnt = 0
+
+
 def PrintToIdxfile(outdir, string, collection ):
+    global idx
     try:
         code = (u"%4.4x"%(ord(string[0])))
     except:
@@ -25,18 +30,73 @@ def PrintToIdxfile(outdir, string, collection ):
             pass
         # this is the old style
         ofile="%s/%s.%s.idx"%(ndir, code, collection)
-        ofile = "/tmp/debug.index"
         try:
             idx[ofile] += "%s\n" % (string[1:])
         except:
             idx[ofile] = "%s\n" % (string[1:])
         
 
+    
+            
 def MandokuIndex(file, idlog='logfile.log', left=2, right=2, length=3, collection='test', use_vol=0):
     pcnt = 0
     defs = {'line' : 0, 'noteflag': 0, 'versflag': 0, 'file': file, 'char': 0, 'para' : 0,
             'txtfile' : os.path.splitext(os.path.split(file)[-1])[0] }
     s=[]
+    #prefill the pre array    
+    pre =  [a for a in ch_re.split(u"　" * left) if len(a) > 0]
+    ##the stack for characters.
+    chars = []
+    ##the stack for inline notes.
+    notes = []
+    ix = 0
+
+    def addtostack(lx):
+        global pcnt
+        lx = lx.replace(' ', '')
+        if len(lx[:-1]) < 1 and pcnt > 1:
+            defs['para'] += 1
+            pcnt = 0
+        elif lx.startswith(u"　　"):
+            defs['para'] += 1
+            pcnt = 0
+        lx = re.sub(u'[~#\u00f1-\u2fff\u3000-\u30FF\uFF00-\uFFEF]', '', lx)
+        ##remove the footnote markers in the hist files
+        lx = re.sub(u'〔[一二三四五六七八九０]+〕', '', lx)
+        lx = re.sub(u'<md[^>]+>', '', lx)
+        lx = re.sub(u'<pb[^>]+>', '', lx)
+        for a in ch_re.split(lx[:-1]):
+            if len(a) > 0:
+                if a == u'¶':
+                    defs['line'] +=  1
+                    defs['char'] = 0
+                else:
+                    defs['char'] += 1
+                    pcnt += 1
+                    chars.append((a, "%s:%s:%d:%d:%d"%(defs['txtfile'], defs['page'], defs['line'], defs['char'], defs['para'])))
+        ## need to disentangle the text now, extract notes into a separate stack if there are some
+        ## notes start by '(', end by ')' and have a possible linebreak '/' within.
+        ## we will collect the notes until we reach a note-end and then spitting them out
+        ## [2010-01-07T13:14:14+0900]
+        ## this needs to be done before the text is output!
+        ## [2010-02-06T17:27:20+0900] FIXME : ZHSJ texts (hist) have the notes where the section is 'b'
+        ## [2010-02-18T20:35:34+0900] and now we want to add the same for verse..
+        ## [2013-08-22T18:36:33+0900] a problem with this seems to be that it mangles complex gaiji expressions
+        ## like [邱-丘+(夸-ㄅ+(万-一))] or maybe the code below handles this?
+        ## [2014-10-24T10:55:24+0900] we are missing lines with more than one "(" here
+        if (lx.find('(') > 0):
+            for i in range(0, len(chars)-1):
+                #look for the note
+                if chars[i][0] == '(':
+                    #throw it away!
+                    chars.pop(i)
+                    defs['noteflag']=1
+                    ##make sure the stack is empty
+                    notes=[]
+                    ix = i
+                    break
+    
+
     def setPage(lx):
         r=lx[lx.find(':')+1:lx.find('>')].split('_')
         defs['ed']=r[1]
@@ -52,13 +112,6 @@ def MandokuIndex(file, idlog='logfile.log', left=2, right=2, length=3, collectio
         #         idlog.write("%(id)s\t%(file)s\t%(ed)s\t%(page)s\t"%(defs))
 
         defs['line']=0
-        
-    pre =  [a for a in ch_re.split(u"　" * left) if len(a) > 0]
-    ##the stack for characters.
-    chars = []
-    ##the stack for inline notes.
-    notes = []
-    ix = 0
     idlog = codecs.open(idlog, 'a', 'utf-8')
     # lets see if we can open the file
     try:
@@ -100,6 +153,14 @@ def MandokuIndex(file, idlog='logfile.log', left=2, right=2, length=3, collectio
             defs['versflag']=1
         elif line.startswith('#+END_VERSE'):
             defs['versflag']=0
+        elif "<md" in line:
+            l1, l2 = line.split("<md", 1)
+            addtostack(l1)
+            setPage(l2)
+            defs['line'] += l2.count(u'¶')
+            defs['char'] = 0
+            addtostack(l2[l2.find('>')+1:])
+            
         elif line.startswith('<pb'):
             ##this assumes a page format of
             ##<pb:SJB_SDZ0001_010002a>
@@ -108,46 +169,7 @@ def MandokuIndex(file, idlog='logfile.log', left=2, right=2, length=3, collectio
             defs['char'] = 0
         elif defs.has_key('page'):
             ## here we go!
-            line = line.replace(' ', '')
-            if len(line[:-1]) < 1 and pcnt > 1:
-                defs['para'] += 1
-                pcnt = 0
-            elif line.startswith(u"　　"):
-                defs['para'] += 1
-                pcnt = 0
-            line = re.sub(u'[~#\u00f1-\u2fff\u3000-\u30FF\uFF00-\uFFEF]', '', line)
-            ##remove the footnote markers in the hist files
-            line = re.sub(u'〔[一二三四五六七八九０]+〕', '', line)
-            line = re.sub(u'<md[^>]+>', '', line)
-            for a in ch_re.split(line[:-1]):
-                if len(a) > 0:
-                    if a == u'¶':
-                        defs['line'] +=  1
-                        defs['char'] = 0
-                    else:
-                        defs['char'] += 1
-                        pcnt += 1
-                        chars.append((a, "%s:%s:%d:%d:%d"%(defs['txtfile'], defs['page'], defs['line'], defs['char'], defs['para'])))
-            ## need to disentangle the text now, extract notes into a separate stack if there are some
-            ## notes start by '(', end by ')' and have a possible linebreak '/' within.
-            ## we will collect the notes until we reach a note-end and then spitting them out
-            ## [2010-01-07T13:14:14+0900]
-            ## this needs to be done before the text is output!
-            ## [2010-02-06T17:27:20+0900] FIXME : ZHSJ texts (hist) have the notes where the section is 'b'
-            ## [2010-02-18T20:35:34+0900] and now we want to add the same for verse..
-            ## [2013-08-22T18:36:33+0900] a problem with this seems to be that it mangles complex gaiji expressions
-            ## like [邱-丘+(夸-ㄅ+(万-一))] or maybe the code below handles this?
-            if (line.find('(') > 0):
-                for i in range(0, len(chars)-1):
-                    #look for the note
-                    if chars[i][0] == '(':
-                        #through it away!
-                        chars.pop(i)
-                        defs['noteflag']=1
-                        ##make sure the stack is empty
-                        notes=[]
-                        ix = i
-                        break
+            addtostack(line)
             while (len(chars) > ix + right + length and defs['noteflag'] == 1):
                 #end of note - we will output it immediately, which is not in
                 #document order, but does not matter.
@@ -163,7 +185,12 @@ def MandokuIndex(file, idlog='logfile.log', left=2, right=2, length=3, collectio
                     notes.append(u"）")
                     notes.extend([a for a in ch_re.split(u"　" * (right -1)) if len(a) > 0])
                     for i in range(1 , len(notes) - right + 1):
-                        s.append((u"%s,%s\t%s\tn"%("".join([a[0] for a in notes[0:length+right]]), "".join(npre),  notes[0][1])))
+                        try:
+                            s.append((u"%s,%s\t%s\tn"%("".join([a[0] for a in notes[0:length+right]]), "".join(npre),  notes[0][1])))
+                        except:
+                            #[2014-10-24T12:37:55+0900]
+                            #maybe notest too short?
+                            print "notes: ", notes, length+right, npre
                         #PrintToIdxfile (outdir, u"%s,%s\t%s\tn\n"%("".join([a[0] for a in notes[0:length+right]]), "".join(npre),  notes[0][1]), collection)
                         npre.append(notes.pop(0)[0])
                         npre.pop(0)
@@ -201,33 +228,14 @@ def MandokuIndex(file, idlog='logfile.log', left=2, right=2, length=3, collectio
                 pre.pop(0)
             except:
                 print "some error occurred: %s %s"%(line, defs), l, length
-#    print "s: ", len(s), s[-1]
-    return s
-#     for f in outfiles.keys():
-#         outfiles[f].close()
-#         #    os.system('gzip '+ outdir + '/*.idx')
-# #    defs['page'] = defs['page'][:-1]
-#     try:
-#         idlog.write("%(page)s_%(line)2.2d\t%(date)s\n"%(defs))
-#     except:
-#         defs['date']= datetime.datetime.now()
-#         try:
-#             idlog.write("%(page)s_%(line)2.2d\t%(date)s\n"%(defs))
-#         except:
-#             idlog.write("some error occurred\n"%(defs))
-        
-#     idlog.close()
 
-#     ##need to write on a per file base.
-#     for of in idx.keys():
-#         outfile=codecs.open(of, 'a+', 'utf-8')
-#         outfile.write(idx[of])
-#         outfile.close()
+    return s
+
 #produce a complete index for one set of branches, return it
 def mdIndexGit(txtdir, repo, branches, left, right, length):
     # branches is a dictionary with branchname and sha value or HEAD
     #TODO: what if no master exists?
-    s=[]
+    alls=[]
     repo.git.checkout(branches['master'])
     for f in os.listdir(txtdir):
         if f.endswith('txt'):
@@ -267,22 +275,25 @@ def mdIndexGit(txtdir, repo, branches, left, right, length):
                         dx = j1 - i1
                         for i in range(i1, i2):
                             varx[i] += " " + b
+            s.extend(varx)
+            alls.append(s)
 #                print "varx: ", " - ".join(varx)
     repo.git.checkout(branches['master'])
-    s.extend(varx)
-    return s
-
+    return alls
+    
 
 def StartIndex(txtdir, idxdir="/tmp/index", left=3, right=3, length=7):
+    global idx
     old = {}
     now = {}
     oldindex = []
     repo = git.Repo(txtdir)
     txtid = os.path.split(txtdir)[-1]
+    coll = txtid[0:4]
     #check if a previous run exists:
     for b in repo.branches:
         now[b.name] = b.commit.hexsha
-    lg = '%s/meta/%s/%s.log' % (idxdir, txtid[0:4], txtid)
+    lg = '%s/meta/%s/%s.log' % (idxdir, coll, txtid)
     # if we have a log file, this is an update
     update = os.path.isfile(lg)
     changed = False
@@ -310,7 +321,7 @@ def StartIndex(txtdir, idxdir="/tmp/index", left=3, right=3, length=7):
             oldindex = mdIndexGit(txtdir, repo, old, left, right, length)
     else:
         try:
-            os.makedirs("%s/meta/%s" % (idxdir, txtid[0:4]))
+            os.makedirs("%s/meta/%s" % (idxdir, coll))
         except:
             pass
     # now we write the new logfile
@@ -322,31 +333,50 @@ def StartIndex(txtdir, idxdir="/tmp/index", left=3, right=3, length=7):
     # check for identical hashes:
     if len(now) > 0:
         index = mdIndexGit(txtdir, repo, now, left, right, length)
-        #print index
-        #        if not update:
-        for i in index:
-            PrintToIdxfile(idxdir, i, txtid[0:4])
+        if not update:
+            if debug:
+                dpath=idxdir + "/debug/" + coll
+                try:
+                    os.makedirs(dpath)
+                except:
+                    pass
+                debfile = codecs.open(dpath + "/" + txtid + ".idx",  "w", "utf-8")
+            for ix in index:
+                for i in ix:
+                    if debug:
+                        debfile.write("%s\n" % ( i))
+                    PrintToIdxfile(idxdir, i, coll)
+                print "writing index %d keys." % (len(idx))
+                for of in idx.keys():
+                    outfile=codecs.open(of, 'a+', 'utf-8')
+                    outfile.write("".join(idx[of]))
+                    outfile.close()
+                idx={}
         else:
             if len(oldindex) > 0:
                 t = SequenceMatcher()
-                t.set_seqs(index, oldindex)
-                for tag, i1, i2, j1, j2 in t.get_opcodes():
-                    print "update: ", tag, i1, i2, j1, j2
-                    if tag == "replace":
-                        
-                    elif tag == "insert":
-                        ##write out procedure to replace or insert
-                    elif tag == "delete":
-                         for i in range(i1, i2):
-                             print index[i]
-        
+                for x in range(0, len(index) - 1):
+                    #this assumes the same number of files...
+                    t.set_seqs(index[x], oldindex[x])
+                    for tag, i1, i2, j1, j2 in t.get_opcodes():
+                        print "update: ", tag, i1, i2, j1, j2
+                        if tag == "replace":
+                            pass
+                            ##write out procedure to replace or insert
+                        elif tag == "insert":
+                            pass
+                            ##write out procedure to replace or insert
+                        elif tag == "delete":
+                            pass
+                             # for i in range(i1, i2):
+                             #     print index[i]
+        try:
+            debfile.close()
+        except:
+            pass
     else:
         return "INFO: Nothing to do, no new commits in repository."
 
-    for of in idx.keys():
-        outfile=codecs.open(of, 'a+', 'utf-8')
-        outfile.write("".join(idx[of]))
-        outfile.close()
     
     ## do sth with the index... : either write out new or write patch
 #    rec.write("hash: \n")
@@ -367,6 +397,12 @@ if __name__ == '__main__':
         os.makedirs(idxdir)
     except:
         pass
+    if debug:
+        try:
+            os.makedirs(idxdir + "/debug")
+        except:
+            pass
+        
     StartIndex(sys.argv[1], idxdir, 3, 3, 7)
 #    MandokuIndex(sys.argv[1], idxdir, idlog, 3, 3, 7, collection, use_vol)
 #    MandokuIndex(sys.argv[1], idxdir, idlog, 2, 2, 5, collection, use_vol)
