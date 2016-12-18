729614e49b8ae5313941059b43feee435474aa18
ChristianW <chris@mbp4-ubuntu.local>
2016-12-19 08:22:25 +0900

    handling for utf-8 output.

--- python/citfind/citfind.py
+++ python/citfind/citfind.py
@@ -4,12 +4,13 @@ from __future__ import division
 from __future__ import absolute_import
 
 import os, sys, codecs, re, datetime, requests
+reload(sys)
+sys.setdefaultencoding('utf-8')
 from difflib import *
 from collections import defaultdict
 ch_re = re.compile(ur'(\[[^\]]*\]|&[^;]*;|&amp;[CZ][X3-7]-[A-F0-9]+|.)')
 img_re = re.compile(ur'<i[^>]*>')
 
-txtbydateurl = "https://raw.githubusercontent.com/kanripo/KR-Workspace/master/Settings/krp-by-date.txt"
 
 url = u"http://dao3.zinbun.kyoto-u.ac.jp:5000/api/v1.0/search?query=%s"
 
@@ -19,32 +20,50 @@ inp = u"學而時習之不亦說乎有朋自遠方來不亦樂乎人不知而不
 # index parameter:
 tbdd={}
 tbdt={}
-pre = 5
-lenx = 20
-n = 2
-cutoff = 0.6
-r = requests.get(txtbydateurl)
-if r.ok:
-    txtbydate = r.text.split("\n")[1:]
-    for tbd in txtbydate:
-        f = tbd.split("\t")
-        try:
-            tbdd[f[0]] = f[1]
-            tbdt[f[0]] = f[2]
-        except:
-            print "oops, ", f
-else:
-    txtbydate = ""
-    
-start = 0
-tally = []
-for start in range(0, len(inp), lenx):
-    cur = inp[start:start+lenx]
-    print cur[pre:pre+n], cur
-    r = requests.get(url % (cur[pre:pre+n]))
-    cur = filter(None, re.split(ch_re, cur))
+
+def cleaninp(s):
+    inzhu=False
+    s=s.split("\n")
+    out=""
+    for line in s:
+        if line.startswith("#"):
+            continue
+        if line.startswith(":zhu:"):
+            inzhu = True
+        if line.startswith(":end:"):
+            inzhu = False
+            continue
+        if inzhu:
+            continue
+        line = line.split("\t")[0]
+        line = re.sub(ur"<[^>]+>|¶", "", line)
+        line = re.sub(ur"[0-9\.-]+\\*", "", line)
+        out += line
+    return ["".join(filter(None, re.split(ur"[\u3000-\u3fff|\uff00-\uffff]", out)))]
+#return filter(None, re.split(ur"[\u3000-\u3fff|\uff00-\uffff]", out))
+
+def getmeta():
+    txtbydateurl = "https://raw.githubusercontent.com/kanripo/KR-Workspace/master/Settings/krp-by-date.txt"
+    r = requests.get(txtbydateurl)
+    if r.ok:
+        txtbydate = r.text.split("\n")[1:]
+        for tbd in txtbydate:
+            f = tbd.split("\t")
+            try:
+                tbdd[f[0]] = f[1]
+                tbdt[f[0]] = f[2]
+            except:
+                print "oops, ", f
+    else:
+        txtbydate = ""
+
+
+def getsec(curr, pre=0, n=2):
+    search = curr[pre:pre+n]
+    r = requests.get(url % (search))
+    curr = filter(None, re.split(ch_re, curr))
     m = SequenceMatcher()
-    m.set_seq1(cur)
+    m.set_seq1(curr)
     res = []
     if r.ok:
         t = r.text.split("\n")[1:]
@@ -56,22 +75,57 @@ for start in range(0, len(inp), lenx):
             if len(tmp) > 2:
                 continue
             s = tmp[0].split(",")
-            txfno = tmp[1].split(":")[0]
-            # for the first l, I would need only the stuff starting with inp[0]
-            if i == 0:
-                l1 = "%s%s" % (cur[pre], s[0])
-            else:
-                try:
-                    l1 = "%s%s%s" % (s[1][-pre:], cur[pre], s[0])
-                except:
-                    l1 = "%s%s" % (cur[pre], s[0])
-            # split and remove empty strings.
-            l1 = filter(None, re.split(ch_re, l1))
-            m.set_seq2(l1)
+            try:
+                txfno = tmp[1].split(":")[0]
+            except:
+                print tmp
+                continue
+            try:
+                l1 = "%s%s%s" % (s[1][-pre:pre], curr[pre], s[0])
+            except:
+                l1 = "%s%s" % (curr[pre], s[0])
+            # split and remove empty strings, trim to the right length
+            l2 = filter(None, re.split(ch_re, l1))[0:len(cur)]
+            #l2 = filter(None, re.split(ch_re, l1))
+            try:
+                ss = s[1][-pre:pre]
+            except:
+                ss = "xx"
+            print search, ss, "".join(l2)
+            m.set_seq2(l2)
             if m.quick_ratio() >= 0.5:
                 print "--", txfno,  "".join(l1), m.ratio()
                 res.append((txfno, l1, m.ratio()))
-    tally.append((cur, res))        
+    return res
+
+pre = 0
+lenx = 10
+n = 2
+cutoff = 0.6
+    
+start = 0
+tally = []
+
+try:
+    inp=codecs.open(sys.argv[1], "r", "utf-8").read()
+except:
+    print "No input file."
+    inp = u"學而時習之不亦說乎有朋自遠方來不亦樂乎人不知而不慍不亦君子乎"
+    
+inp = cleaninp(inp)
+for cur in inp:
+    if len(cur) > lenx:
+        for start in range(0, len(cur), lenx):
+            curx = cur[start:start+lenx]
+            print curx
+            res = getsec(curx, pre, n)
+            tally.append((curx, res))
+    elif (len(cur) > 2):
+        print cur
+        res = getsec(cur, pre, n)
+        tally.append((cur, res))
+    else:
+        print "Nothing", cur
         # maybe rather compare lists / sets
         # list(set(temp1) - set(temp2)) # diff
         # set(a).intersection(b)  # intersection
@@ -97,7 +151,8 @@ for ta in tally:
         txt = e[0].split("_")[0]
         rt = e[-1]
         if rt > cutoff:
-            rs[txt] += 1
-
+            rs[txt] += rt
+getmeta()
 for r in rs:
-    print rs[r], r
+    print rs[r], r, tbdd[r], tbdt[r]
+    
