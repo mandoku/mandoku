c73efe594fae408fe535a898735ae89179ecc0c1
ChristianW <chris@mbp4-ubuntu.local>
2017-01-23 14:31:58 +0900

    revised mandoku-init for sinomacs.

--- lisp/mandoku-init.el
+++ lisp/mandoku-init.el
@@ -11,53 +11,54 @@
 
 ;;; 
 ;;; you should know what you are doing if you change anything below...
+(unless (boundp 'sinomacs-dir)
+  ;; looks like we are not in sinomacs, so we will do this ourselves...
+  (require 'package)
+  (setq package-enable-at-startup nil)
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.org/packages/"))
+  (setq use-package-always-ensure t)
 
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/"))
-(setq use-package-always-ensure t)
+  (package-initialize)
 
-(package-initialize)
+  ;; Bootstrap `use-package'
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+  (require 'use-package)
 
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
+  (use-package magit
+    :bind ("C-x g" . magit-status)
+    :ensure t)
 
-(use-package magit
-  :bind ("C-x g" . magit-status)
-  :ensure t)
+  (use-package gh
+    :ensure t)
 
-(use-package gh
-  :ensure t)
+  (use-package github-clone
+    :init (setq github-clone-url-slot :clone-url)
+    :ensure t)
 
-(use-package github-clone
-  :init (setq github-clone-url-slot :clone-url)
-  :ensure t)
+  (use-package mandoku
+    :init (setq org-return-follows-link t)
+    (setq org-startup-folded 'showeverything)
+    (setq mandoku-do-remote t)
+    (setq mandoku-string-limit 10)
+    (setq mandoku-index-display-limit 2000)
+    (setq mandoku-repositories-alist '(("KR" "http://www.kanripo.org/api/v1.0") ))
+    (autoload 'mandoku-view-mode "mandoku" nil t)
+    :config
+    (define-key mandoku-view-mode-map (kbd "C-c i")  'mandoku-open-image-at-page)
+    (define-key mandoku-view-mode-map (kbd "C-c d")  'mandoku-get-remote-text-now)
 
-(use-package mandoku
-  :init (setq org-return-follows-link t)
-  (setq org-startup-folded 'showeverything)
-  (setq mandoku-do-remote t)
-  (setq mandoku-string-limit 10)
-  (setq mandoku-index-display-limit 2000)
-  (setq mandoku-repositories-alist '(("KR" "http://www.kanripo.org/api/v1.0") ))
-  (autoload 'mandoku-view-mode "mandoku" nil t)
-  :config
-  (define-key mandoku-view-mode-map (kbd "C-c i")  'mandoku-open-image-at-page)
-  (define-key mandoku-view-mode-map (kbd "C-c d")  'mandoku-get-remote-text-now)
-
-  :bind (("<f6>" . mandoku-search-text)
+    :bind (("<f6>" . mandoku-search-text)
 	 ("<f7>"  . mandoku-search-titles)  )
+    )
+
+  (use-package guide-key
+    :defer 5
+    :diminish guide-key-mode
+    :config
+    (progn
+      (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-c"))
+      (guide-key-mode 1)))  ; Enable guide-key-mode
 )
-
-(use-package guide-key
-  :defer 5
-  :diminish guide-key-mode
-  :config
-  (progn
-  (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-c"))
-  (guide-key-mode 1)))  ; Enable guide-key-mode
-
