f993b7428c7e83efdbc6e6040d3eae9406d9b090
ChristianW <cwittern@yahoo.com>
2017-12-27 08:31:32 +0900

    changed

--- python/index/bzip-coll.sh
+++ python/index/bzip-coll.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 #cd ~/db/index
-for file in */*.idx
+for dir in *
 do
-    tmp=$(wc -c "$file" )
-    FILESIZE=`echo $tmp | cut -d " " -f 1`
-    if [ $FILESIZE -gt 500 ]; then
-    	bzip2 --best $file
-    fi
-    echo $FILESIZE
+    for file in $dir/*/*.idx
+    do
+	tmp=$(wc -c "$file" )
+	FILESIZE=`echo $tmp | cut -d " " -f 1`
+	if [ $FILESIZE -gt 500 ]; then
+    	    bzip2 --best $file
+	    # echo "big"
+	fi
+	echo $FILESIZE
+    done
 done
